d9cedd3469c30a5beed4ec5b9adf627d1526705f
viniciuspessoni <Ch@conete7>
2020-11-15 10:58:44 +0000

    Exemplo de uso do Selenium com Java e Hamcrest apenas

--- src/test/java/ExemploSimplesUmaPagina.java
+++ src/test/java/ExemploSimplesUmaPagina.java
@@ -1,27 +1,16 @@
-import org.hamcrest.core.StringContains;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class ExemploSimplesUmaPagina {
 
 
-    @Test
-    @DisplayName("Quando abrir página principal do Tester Global, Então encontrar o título da página presente.")
-    public void verificaPaginaPrincipal () {
-        /**
-         *  Assumimos que o chromedriver esteja na sua pasta de sistema corretamente.
-         *  ex:
-         *      Windows: C:/users/seuUsuario
-         *      MAC: usr/local/bin
-         */
+    public static void main (String[] args) {
+
         WebDriver driver = new ChromeDriver();
 
         String baseUrl = "http://www.viniciuspessoni.com";
@@ -31,6 +20,7 @@ public class ExemploSimplesUmaPagina {
         String tituloEncontrado = "";
 
         driver.get(baseUrl);
+
         tituloEncontrado = driver.findElement(By.cssSelector(".et_pb_text_inner")).getText();
 
         System.out.println(tituloEncontrado);
@@ -41,8 +31,10 @@ public class ExemploSimplesUmaPagina {
          * Observe ainda que o matcher vem do Hamcrest. Usamos bibliotecas específicas para nos ajudar
          * a ter matchers mais robustos.
          */
-        assertThat(tituloEncontrado, containsString(tituloEsperado));
+       MatcherAssert.assertThat(tituloEncontrado, Matchers.containsString(tituloEsperado));
 
-        driver.close();
+        // Observe que se estamos usando só o Java pra rodar o selenium, precisamos usar o quit ao inves do .close aqui
+        driver.quit();
+
     }
 }
